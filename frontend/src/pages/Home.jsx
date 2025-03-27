caf5679e55a415814e9f917d9cbe4a2d0e955bea
_.mindang <minhdang875425@gmail.com>
2025-03-27 07:39:04 +0700

    merge main

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import '../styles/Home.css'
 
 
+import banner from '../assets/banner.png'
+
 export default function Home() {
 
     return (
-        <div className='home'>
+        <div className='home'>   
+            <div className='banner'>
+                <img src={banner}></img>
+            </div>
+            <Display/>
+            <Pagination
+                totalProducts={phone.length} 
+                productPerPages={productPerPages}
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+            />
         </div>
     )
 }
