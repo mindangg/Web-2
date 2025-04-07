251b427e780f638b726060aad9640c1b245e1b4a
_.mindang <minhdang875425@gmail.com>
2025-04-07 17:37:38 +0700

    employee admin, responsive

--- frontend/src/pages/Admin.jsx
+++ frontend/src/pages/Admin.jsx
@@ -19,46 +19,39 @@ export default function Admin() {
 
     const [toggle, setToggle] = useState('user')
 
-    const [showSideNav, setShowSideNav] = useState(false);
+    const [showHeader, setShowHeader] = useState(false)
+    const [showSideNav, setShowSideNav] = useState(true)
 
 
     return admin ? (
     <div>
-        <button
-            className="hamburger-btn"
-            onClick={() => setShowSideNav(prev => !prev)}
-        >
-            <i className="fa-solid fa-bars"></i>
-        </button>
-
-        {/* Optional overlay to click outside and close sidenav */}
+        <div className='admin-header'>
+            <div>
+                <img src={logo} alt='Logo' />
+            </div>
+            <div className='toggleBar' onClick={() => setShowSideNav(!showSideNav)}>
+                <i className="fa-solid fa-bars"></i>
+            </div>
+        </div>
         {/* {showSideNav && (
             <div className="overlay" onClick={() => setShowSideNav(false)}></div>
         )} */}
 
         <div className={`sidenav ${showSideNav ? 'show' : 'hide'}`}>
-            <div>
-                <div className='topnav'>
-                        <img src={logo} alt='Logo' />
-                </div>
-                <div className='middlenav'>
-                    <ul>
-                        <li onClick={() => setToggle('user')}><i className='fa-solid fa-users'></i> Người dùng</li>
-                        <li onClick={() => setToggle('employee')}><i className='fa-solid fa-user-tie'></i> Nhân viên</li>
-                        <li onClick={() => setToggle('product')}><i className='fa-solid fa-book'></i> Sản phẩm</li>
-                        <li onClick={() => setToggle('supplier')}><i className='fa-solid fa-truck-field'></i> Kho hàng</li>
-                        <li onClick={() => setToggle('order')}><i className='fa-solid fa-basket-shopping'></i> Đơn hàng</li>
-                        <li onClick={() => setToggle('order-statistic')}><i className='fa-solid fa-chart-simple'></i> Thống kê</li>
-                    </ul>
-                </div>
-            </div>
-            <div className='bottomnav'>
-                <ul>
-                    <li><i className="fa-solid fa-pen-ruler"></i> {admin.employee.role_name}</li>
-                    <li><i className='fa-regular fa-circle-user'></i> {admin.employee.full_name}</li>
-                    <li onClick={logout}><i className='fa-solid fa-arrow-right-from-bracket'></i> Logout</li>
-                </ul>
-            </div>
+            <ul>
+                <li onClick={() => setToggle('user')} style={{paddingTop: '0px'}}><i className='fa-solid fa-users'></i> Người dùng</li>
+                <li onClick={() => setToggle('employee')}><i className='fa-solid fa-user-tie'></i> Nhân viên</li>
+                <li onClick={() => setToggle('product')}><i className='fa-solid fa-book'></i> Sản phẩm</li>
+                <li onClick={() => setToggle('supplier')}><i className='fa-solid fa-truck-field'></i> Kho hàng</li>
+                <li onClick={() => setToggle('order')}><i className='fa-solid fa-basket-shopping'></i> Đơn hàng</li>
+                <li onClick={() => setToggle('order-statistic')}><i className='fa-solid fa-chart-simple'></i> Thống kê</li>
+            </ul>
+            
+            <ul>
+                <li><i className="fa-solid fa-pen-ruler"></i> {admin.employee.role_name}</li>
+                <li><i className='fa-regular fa-circle-user'></i> {admin.employee.full_name}</li>
+                <li onClick={logout}><i className='fa-solid fa-arrow-right-from-bracket'></i> Logout</li>
+            </ul>
         </div>
         {/* <div className ="menu">
             <div>Hệ thống quản trị</div>
