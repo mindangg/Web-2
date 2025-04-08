7e6fdedcb60cdcb912d52bb2c4a053508cc17fed
Hbao05 <dotranhuybao2005@gmail.com>
2025-04-08 15:04:53 +0700

    Fix merge conflicts

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')).render(
         <AuthContextProvider>
           <NotificationContextProvider>
             <CardDetailsContextProvider>
-              <CartProvider>
+              <CartProvider> 
                 <App />
               </CartProvider>
             </CardDetailsContextProvider>
